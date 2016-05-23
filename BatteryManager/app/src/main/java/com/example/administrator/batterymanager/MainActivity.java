952f52bb189b5a2e36fca30bacc0ce0e17c934e8
Fishball_Lin <yu9622@gmail.com>
2016-05-23 16:13:02 +0800

    Add broadcast receiver by dynamically registering

--- BatteryManager/app/src/main/java/com/example/administrator/batterymanager/MainActivity.java
+++ BatteryManager/app/src/main/java/com/example/administrator/batterymanager/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.administrator.batterymanager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BroadcastReceiver batteryReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        batteryReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+            }
+        };
+    }
+
+    @Override
+    protected  void onResume(){
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(batteryReceiver, intentFilter);
+    }
+
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        unregisterReceiver(batteryReceiver);
     }
 }
